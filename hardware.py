3dad55e216607dd717872089a21c9570103ff760
amanias <git@tuinformaticoencasa.com>
2022-10-07 15:09:46 +0200

    Modificar comentarios para no ejecutar funciones hijas

--- hardware.py
+++ hardware.py
@@ -6,13 +6,13 @@ def chequear_disco(disk):
     """Verificar que hay suficiente espacio libre en disco"""
     espacioEnDisco = shutil.disk_usage(disk)
     libre = espacioEnDisco.free/espacioEnDisco.used * 100
-#    print("Espacio en disco: {:f}".format(libre))
+    #print("Espacio en disco: {:f}".format(libre))
     return libre > 20
 
 def chequear_uso_cpu():
     """Verificar que la carga de procesos de la CPU es suficiente"""
     uso = psutil.cpu_percent(1)
-#    print("Uso del procesador: {:f} %".format(uso))
+    #print("Uso del procesador: {:f} %".format(uso))
     return uso < 75
 
 #Realización de las comprobaciones

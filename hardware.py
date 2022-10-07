f46ac8dd40fbf33f6866450bfdc91c13a1486292
amanias <git@tuinformaticoencasa.com>
2022-10-07 15:58:07 +0200

    Activar los chequeos Hardware en local

--- hardware.py
+++ hardware.py
@@ -6,17 +6,17 @@ def chequear_disco(disk):
     """Verificar que hay suficiente espacio libre en disco"""
     espacioEnDisco = shutil.disk_usage(disk)
     libre = espacioEnDisco.free/espacioEnDisco.used * 100
-    #print("Espacio en disco: {:f}".format(libre))
+    print("Espacio en disco: {:f}".format(libre))
     return libre > 20
 
 def chequear_uso_cpu():
     """Verificar que la carga de procesos de la CPU es suficiente"""
     uso = psutil.cpu_percent(1)
-    #print("Uso del procesador: {:f} %".format(uso))
+    print("Uso del procesador: {:f} %".format(uso))
     return uso < 75
 
 #Realización de las comprobaciones
-#if not chequear_disco("C:/") or not chequear_uso_cpu():
-    #print("ERROR de Hardware!!!")
-#else:
-    #print("Todo correcto!!!")
+if not chequear_disco("C:/") or not chequear_uso_cpu():
+    print("ERROR de Hardware!!!")
+else:
+    print("Todo correcto!!!")
